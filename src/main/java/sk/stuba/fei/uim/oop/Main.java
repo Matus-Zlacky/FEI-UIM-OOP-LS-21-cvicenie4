0a2f9a5f3ba03a9134938c32cc445fd088e329d4
Jakub Kovář <jakub.kovar@stuba.sk>
2021-03-08 13:45:49 +0100

    object references

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -5,7 +5,17 @@ public class Main {
     public static void main(String[] args) {
         Contact contact = new Contact("Jakub", "Kovar");
         var contact2 = new Contact("Martin", "Kranec");
-        var cislo = 5;
-        cislo = contact2;
+        var contact3 = new Contact("Tomas Kovacik");
+
+        var contact4 = Contact.parseFromFullName("Gabriel Juhas");
+
+        var contacts1 = new Contact[] {contact2, contact3, contact4};
+        contact.setFriendsFromArray(contacts1);
+
+        contacts1[0].setFirstName("Mato");
+
+        contact.setFriends(contact4, contact2, contact3);
+
+        contact3.setFirstName("Kovy");
     }
 }
